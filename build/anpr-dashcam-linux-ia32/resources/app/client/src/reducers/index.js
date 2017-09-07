e60460be53218aa8d6f1b50828693d77fff19ce3
Ed Kelly <ekelly@sapient.com>
2017-09-07 16:42:37 +0100

    Removing builds

--- build/anpr-dashcam-linux-ia32/resources/app/client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import details from './details';
-import image from './image';
-
-export default combineReducers({
-  details,
-  image,
-});
--- build/anpr-dashcam-linux-ia32/resources/app/constants/events.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  IMAGE_SEND: 'IMAGE_SEND',
-  DETAILS_SEND: 'DETAILS_SEND',
-};
